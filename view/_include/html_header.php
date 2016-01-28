75b48b616f5c41a5eb2dcebc1c45ce43718da677
AMV_Ph34r <AMVPh34r@gmail.com>
2016-01-28 17:34:55 -0500

    Add 404 page, make relative links absolute
    Links to images, CSS/JS files, etc should now be prefixed with "/"
    to make sure even if the user is at X/Y/page they show correctly.
    I THINK this is the best way to do things, but there might be a better
    way.

--- view/_include/html_header.php
+++ view/_include/html_header.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo CONFIG::$site_title; ?></title>
-		<link rel="shortcut icon" sizes="16x16 32x32 48x48 64x64" href="img/favicon.ico">
-		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
-		<!--[if IE]><link rel="shortcut icon" href="img/favicon.ico"><![endif]-->
+		<link rel="shortcut icon" sizes="16x16 32x32 48x48 64x64" href="/img/favicon.ico">
+		<link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
+		<!--[if IE]><link rel="shortcut icon" href="/img/favicon.ico"><![endif]-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<link rel="stylesheet" href="css/bootstrap.min.css" />
-		<link rel="stylesheet" href="css/style.min.css" />
+		<link rel="stylesheet" href="/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="/css/style.min.css" />
 	</head>
 <body>
---
+++ view/page/404.php
@@ -0,0 +1,15 @@
+<?php Template::includeTemplate('html_header.php'); ?>
+<div class="background"></div>
+<div class="container-fluid center-box">
+	<div class="row-fluid">
+		<div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 text-center" id="mainContent">
+			<h1>404</h1>
+			<p>Sorry, I couldn't find that page!</p>
+			<p><a href="/index">Click here</a> to return home and try again, or to contact me if you have any trouble.</p>
+			<div class="panel-group" id="socialPanels"></div>
+		</div>
+		<div class="copyright col-lg-12 text-center">&copy;2014-2015 Alex Schaeffer</div>
+	</div>
+</div>
+
+<?php Template::includeTemplate('html_footer.php'); ?>
