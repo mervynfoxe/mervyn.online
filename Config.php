b31fdc4305aed96c454eaa53fa4447f9af3092f2
AMV_Ph34r <AMVPh34r@gmail.com>
2016-02-11 18:39:32 -0500

    Set channel name in a controller variable
    To make it easy to change in the future if the need arises.
    Also changed other variable names for a consistent naming scheme.

--- Config.php
+++ Config.php
@@ -1,7 +1,7 @@
 <?php
 
 class Config {
-	public static $site_title = 'AMV_Ph34r';
-	public static $log_dir = 'logs';
-	public static $dateFormat = 'Y-m-d\TH-i-s'; // Standard XML time format.
+	public static $sSiteTitle = 'AMV_Ph34r';
+	public static $sLogDir = 'logs';
+	public static $sDateFormat = 'Y-m-d\TH-i-s'; // Standard XML time format.
 }
