2bf3284613c3aef69dfb118dcaf4607612a3efb0
AMV_Ph34r <AMVPh34r@gmail.com>
2016-02-12 20:19:00 -0500

    Change links to Twitch/home to be internal

--- view/_include/twitch/navbar.php
+++ view/_include/twitch/navbar.php
@@ -4,7 +4,7 @@
 			<span class="sr-only">Toggle navigation</span>
 			<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="/twitch">Home</a>
+		<a class="navbar-brand" href="/">Home</a>
 	</div>
 	<div class="collapse navbar-collapse" id="navbar-collapse">
 		<ul class="nav navbar-nav">
