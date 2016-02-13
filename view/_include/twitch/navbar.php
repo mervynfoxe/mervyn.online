b7e93f3528a61b90b1454465a22f40415ba32cb6
AMV_Ph34r <AMVPh34r@gmail.com>
2016-02-12 20:35:48 -0500

    Add logo to Twitch page navbar

--- view/_include/twitch/navbar.php
+++ view/_include/twitch/navbar.php
@@ -4,7 +4,7 @@
 			<span class="sr-only">Toggle navigation</span>
 			<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="/">Home</a>
+		<a class="navbar-brand" href="/"><img src="/img/logo-a.png" />Home</a>
 	</div>
 	<div class="collapse navbar-collapse" id="navbar-collapse">
 		<ul class="nav navbar-nav">
