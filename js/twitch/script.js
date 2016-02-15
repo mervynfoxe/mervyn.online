af3a65baa28036c8213eebf6d6f3f1724e5e8dc8
AMV_Ph34r <AMVPh34r@gmail.com>
2016-02-15 18:40:23 -0500

    Show poll on page load if requested by hash

--- js/twitch/script.js
+++ js/twitch/script.js
@@ -1,21 +1,31 @@
-var containerWidth = $(".wrapper").width();
-var padding = 0;
-
-/*$(".panel")
-	.mousedown(function() {
-		// $(this).find(".mask").show();
-	})
-	.mouseup(function() {
-		// $(this).find(".mask").hide();
-	})
-	.resizable({
-	handles: 'e',
-	containment: 'parent',
-	resize: function(event, ui){
-		var currentWidth = ui.size.width;
-		console.log(containerWidth - currentWidth);
-		// set the content panel width
-		$(".panel").width(containerWidth - currentWidth);
-		$(this).width(currentWidth);
+$(function() {
+	if(window.location.hash === "#poll") {
+		$("#poll-modal")
+			.modal('show')
+			.on('hide.bs.modal', function() {
+				window.location.hash = "";
+			});
 	}
-});*/
+
+	/*var containerWidth = $(".wrapper").width();
+	var padding = 0;
+
+	$(".panel")
+		.mousedown(function() {
+			// $(this).find(".mask").show();
+		})
+		.mouseup(function() {
+			// $(this).find(".mask").hide();
+		})
+		.resizable({
+		handles: 'e',
+		containment: 'parent',
+		resize: function(event, ui){
+			var currentWidth = ui.size.width;
+			console.log(containerWidth - currentWidth);
+			// set the content panel width
+			$(".panel").width(containerWidth - currentWidth);
+			$(this).width(currentWidth);
+		}
+	});*/
+});
