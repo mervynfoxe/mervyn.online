568164bff6b8917cc9977e7a59576a8a12497534
AMV_Ph34r <AMVPh34r@gmail.com>
2016-05-09 18:09:16 -0400

    Make social icons consistent, move to own dir

--- view/page/index.php
+++ view/page/index.php
@@ -10,11 +10,11 @@
 			<h1>Alex Schaeffer</h1>
 			<p>Web Developer | Programmer | Slacker</p>
 			<div id="social">
-				<span><a href="https://steamcommunity.com/id/AMV-Ph34r/" id="steam-link" title="AMV_Ph34r on Steam" target="_blank"><img src="/img/icon-steam.png" alt="steam" /></a></span>
-				<span><a href="https://twitter.com/AMV_Ph34r" id="twitter-link" title="@AMV_Ph34r on Twitter" target="_blank"><img src="/img/icon-twitter.png" alt="twitter" /></a></span>
-				<span><a href="https://www.youtube.com/user/AMVPh34r" id="youtube-link" title="AMVPh34r on YouTube" target="_blank"><img src="/img/icon-youtube.png" alt="youtube" /></a></span>
-				<span><a href="/twitch" id="twitch-link" title="AMVPh34r on Twitch"><img src="/img/icon-twitch.png" alt="twitch" /></a></span>
-				<span><a href="https://www.linkedin.com/in/arschaeffer" id="linkedin-link" title="Alex Schaeffer on LinkedIn" target="_blank"><img src="/img/icon-linkedin.png" alt="linkedin" /></a></span>
+				<span><a href="https://steamcommunity.com/id/AMV-Ph34r/" id="steam-link" title="AMV_Ph34r on Steam" target="_blank"><img src="/img/social/icon-steam.png" alt="steam" /></a></span>
+				<span><a href="https://twitter.com/AMV_Ph34r" id="twitter-link" title="@AMV_Ph34r on Twitter" target="_blank"><img src="/img/social/icon-twitter.png" alt="twitter" /></a></span>
+				<span><a href="https://www.youtube.com/user/AMVPh34r" id="youtube-link" title="AMVPh34r on YouTube" target="_blank"><img src="/img/social/icon-youtube.png" alt="youtube" /></a></span>
+				<span><a href="/twitch" id="twitch-link" title="AMVPh34r on Twitch"><img src="/img/social/icon-twitch.png" alt="twitch" /></a></span>
+				<span><a href="https://www.linkedin.com/in/arschaeffer" id="linkedin-link" title="Alex Schaeffer on LinkedIn" target="_blank"><img src="/img/social/icon-linkedin.png" alt="linkedin" /></a></span>
 			</div>
 			<div class="panel-group" id="socialPanels"></div>
 		</div>
