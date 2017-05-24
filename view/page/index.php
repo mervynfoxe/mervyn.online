698159e87facc2614f3d9500fbfe50a4faf0c74e
AMV_Ph34r <AMVPh34r@gmail.com>
2017-05-24 10:39:08 -0400

    Add "other" social links

--- view/page/index.php
+++ view/page/index.php
@@ -21,12 +21,42 @@
 					<span><a href="https://twitter.com/AMV_Ph34r" id="twitter-link" title="@AMV_Ph34r on Twitter" target="_blank"><img src="/img/social/icon-twitter.png" alt="twitter" /></a></span>
 					<span><a href="https://www.youtube.com/user/AMVPh34r" id="youtube-link" title="AMVPh34r on YouTube" target="_blank"><img src="/img/social/icon-youtube.png" alt="youtube" /></a></span>
 					<span><a href="/twitch" id="twitch-link" title="AMVPh34r on Twitch"><img src="/img/social/icon-twitch.png" alt="twitch" /></a></span>
+					<span><a href="#panel-other-links" id="other-links" title="Me elsewhere" data-toggle="collapse" data-parent="#socialPanels"><img src="/img/social/icon-other.png" alt="other" /></a></span>
 				<?php elseif (strpos(PATH::$HOST, 'arschaeffer') !== false): ?>
 					<span><a href="http://static.amv-ph34r.com/files/resume.pdf" id="resume-link" title="My Resume" target="_blank"><img src="/img/social/icon-document.png" alt="resume" /></a></span>
 					<span><a href="https://www.linkedin.com/in/arschaeffer" id="linkedin-link" title="Alex Schaeffer on LinkedIn" target="_blank"><img src="/img/social/icon-linkedin.png" alt="linkedin" /></a></span>
 				<?php endif; ?>
 			</div>
-			<div class="panel-group" id="socialPanels"></div>
+			<div class="panel-group" id="socialPanels">
+				<?php if (strpos(PATH::$HOST, 'amv-ph34r') !== false): ?>
+					<div class="panel panel-default">
+						<div id="panel-other-links" class="panel-collapse collapse">
+							<div class="panel-body flex-container">
+								<div class="flex-item">
+									<strong>Tumblr</strong><br />
+									<a href="http://tumblr.amv-ph34r.com/" target="_blank">amv-ph34r</a>
+								</div>
+								<div class="flex-item">
+									<strong>Discord</strong><br />
+									AMV_Ph34r#4895
+								</div>
+								<div class="flex-item">
+									<strong>Battle.net</strong><br />
+									AMVPh34r#1898
+								</div>
+								<div class="flex-item">
+									<strong>3DS</strong><br />
+									4012-4350-8803
+								</div>
+								<div class="flex-item">
+									<strong>Switch</strong><br />
+									SW-6318-7125-1032
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endif; ?>
+			</div>
 		</div>
 		<?php Template::includeTemplate('copyright.php'); ?>
 	</div>
