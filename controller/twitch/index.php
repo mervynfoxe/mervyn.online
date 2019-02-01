6f23458dde4f8271a57e39f63343f474b515b8df
AMV_Ph34r <AMVPh34r@gmail.com>
2019-02-01 10:15:31 -0500

    Update Twitch links

--- controller/twitch/index.php
+++ controller/twitch/index.php
@@ -3,6 +3,6 @@
 	*	Set config variables for the page
 	*/
 	Template::set('sPageTitle', 'Twitch');
-	Template::set('sTwitchChannel', 'amvph34r');
+	Template::set('sTwitchChannel', 'mervynfoxe');
 	Template::set('discord_invite', '0WVs7nLk0ANpbfiq');
 	Template::set('discord_enabled', FALSE);
