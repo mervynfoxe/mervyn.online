64d632de4c2c48962a4eed30b8bd4ec342bee040
Mervyn Fox <AMVPh34r@gmail.com>
2021-08-19 14:27:13 -0400

    No more dead(-ish)name on non-professional page :)

--- view/_include/copyright.php
+++ view/_include/copyright.php
@@ -1,7 +1,7 @@
 <div class="copyright col-lg-12 text-center">
     &copy;2014-<?= date("Y") ?>
 	<?php if (Config::$sCurrentEnv == 'public'): ?>
-        <span class="tooltip-enable" title="Alex Schaeffer">Mervyn Fox</span>
+        <span class="tooltip-enable">Mervyn Fox</span>
 	<?php else: ?>
         Alex Schaeffer
 	<?php endif; ?>
