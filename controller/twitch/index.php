88f4508f4a11ca5d94403c7414a1098d46bcb327
Mervyn Fox <mervynfoxe@gmail.com>
2024-03-22 12:38:21 -0400

    Install Laravel framework

--- controller/twitch/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*
-	*	Set config variables for the page
-	*/
-	Template::set('sPageTitle', 'Twitch');
-	Template::set('sTwitchChannel', 'mervynfoxe');
-	Template::set('discord_invite', '0WVs7nLk0ANpbfiq');
-	Template::set('discord_enabled', FALSE);
