69502cfaff03427656b044f565b50d4a28f2ff72
Mervyn Fox <mervynfoxe@gmail.com>
2024-09-25 12:26:57 -0400

    Add sidebar icon/link to feed

--- resources/views/vendor/prezet/components/sidebar.blade.php
+++ resources/views/vendor/prezet/components/sidebar.blade.php
@@ -13,7 +13,7 @@
 </div>
 
 {{-- Desktop Sidebar --}}
-<div class="hidden lg:relative lg:block lg:flex-none">
+<div class="sidebar hidden lg:relative lg:block lg:flex-none">
     <div class="absolute inset-y-0 right-0 w-[50vw] bg-gray-50"></div>
     <div
         class="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-gray-800"
@@ -23,7 +23,12 @@
         class="sticky top-[4.75rem] -ml-0.5 flex h-[calc(100vh-4.75rem)] w-64 flex-col justify-between overflow-y-auto overflow-x-hidden pb-4 pl-0.5 pr-8 pt-16 xl:w-72 xl:pr-16"
     >
         <x-prezet::nav :nav="$nav" />
-        <div class="mt-16 text-xs text-gray-400">
+        <div class="sidebar-footer mt-16 text-xs text-gray-400">
+            <div class="sidebar-icons mb-2 text-xs text-gray-400">
+                <a href="{{ route('feeds.main') }}">
+                    <img class="sidebar-icon" src="{{ Vite::asset('resources/images/icons/rss.svg') }}" alt="RSS feed icon" />
+                </a>
+            </div>
             <p>&copy;2014-<?= date("Y") ?> Ren Mervyn Fox</p>
             <p><a target="_blank" href="https://prezet.com">Powered by Prezet</a></p>
         </div>
