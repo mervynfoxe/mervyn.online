b69f280ecf5a64ab4c07674cd3bcff74543e0bb5
Mervyn Fox <mervynfoxe@gmail.com>
2025-07-14 09:14:47 -0400

    Properly generate og:image url if needed
    This will more consistently generate SEO image tags for featured images,
    especially in cases where S3 or alternative storage configs are used.

--- app/Http/Controllers/Prezet/ShowController.php
+++ app/Http/Controllers/Prezet/ShowController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Prezet;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 use Prezet\Prezet\Prezet;
 
@@ -16,10 +17,12 @@ class ShowController
         $html = Prezet::parseMarkdown($md)->getContent();
         $headings = Prezet::getHeadings($html);
         $docData = Prezet::getDocumentDataFromFile($doc->filepath);
+        $image = $docData->frontmatter->image ? Storage::disk(Prezet::getPrezetDisk())->url('images/' . $docData->frontmatter->image) : NULL;
         $linkedData = json_encode(Prezet::getLinkedData($docData), JSON_UNESCAPED_SLASHES);
 
         return view('prezet.show', [
             'document' => $docData,
+            'image' => $image,
             'linkedData' => $linkedData,
             'headings' => $headings,
             'body' => $html,
