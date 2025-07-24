062721f76468bec786fdc097b86e1688fdcbf24b
Mervyn Fox <mervynfoxe@gmail.com>
2025-07-24 11:04:57 -0400

    Conditionally show author bio
    Also hide featured image below md (still disabled)

--- resources/views/prezet/show.blade.php
+++ resources/views/prezet/show.blade.php
@@ -42,27 +42,31 @@
                 </h1>
                 <ul class="flex flex-wrap items-center gap-3 font-medium">
                     <li class="w-full sm:w-auto dark:text-white">
-                        <a
-                            href="#author"
-                            class="group flex items-center gap-x-2"
-                        >
-                            @if($author['image'])
-                                <img
-                                    src="{{ $author['image'] }}"
-                                    alt="{{ $author['name'] }} profile image"
-                                    width="26"
-                                    height="26"
-                                    loading="lazy"
-                                    decoding="async"
-                                    class="h-[26px] w-[26px] rounded bg-zinc-100 object-cover transition-all duration-300 group-hover:opacity-75 dark:bg-zinc-800"
-                                />
-                            @endif
-                            <span
-                                class="group-hover:text-primary transition-all duration-300"
+                        @if($author['bio'])
+                            <a
+                                href="#author"
+                                class="group flex items-center gap-x-2"
                             >
-                                {{ $author['name'] }}
-                            </span>
-                        </a>
+                        @endif
+                        @if($author['image'])
+                            <img
+                                src="{{ $author['image'] }}"
+                                alt="{{ $author['name'] }} profile image"
+                                width="26"
+                                height="26"
+                                loading="lazy"
+                                decoding="async"
+                                class="h-[26px] w-[26px] rounded bg-zinc-100 object-cover transition-all duration-300 group-hover:opacity-75 dark:bg-zinc-800"
+                            />
+                        @endif
+                        <span
+                            class="group-hover:text-primary transition-all duration-300"
+                        >
+                            {{ $author['name'] }}
+                        </span>
+                        @if($author['bio'])
+                            </a>
+                        @endif
                     </li>
                     <li
                         class="hidden text-zinc-600 sm:inline-block dark:text-zinc-400"
@@ -80,7 +84,7 @@
             {{-- Hero Image --}}
             {{-- TMP: disable this --}}
             @if ($image && false)
-                <div class="-mx-8 sm:mx-0 col-span-8 col-start-3 lg:col-span-10 lg:col-start-2 2xl:col-start-3 lg:my-4 lg:max-w-3/4 xl:max-w-1/2">
+                <div class="hidden md:block -mx-8 sm:mx-0 col-span-8 col-start-3 lg:col-span-10 lg:col-start-2 2xl:col-start-3 lg:my-4 lg:max-w-3/4 xl:max-w-1/2">
                     <img
                     src="{{ $image }}"
                     alt=""
@@ -194,56 +198,58 @@
                         </ul>
                     @endif
                 </div>
-                <div
-                    id="author"
-                    class="flex flex-col items-start gap-x-6 gap-y-4 rounded-xl bg-zinc-50 p-6 ring-1 ring-zinc-500/10 ring-inset md:flex-row md:p-7 dark:bg-zinc-800 dark:text-zinc-300"
-                >
-                    @if($author['image'])
-                        <img
-                            src="{{ $author['image'] }}"
-                            alt="profile image of {{ $author['name'] }}"
-                            width="135"
-                            height="135"
-                            loading="lazy"
-                            decoding="async"
-                            class="h-24 w-24 rounded-xl bg-zinc-100 object-cover md:h-[135px] md:w-[135px] dark:bg-zinc-800"
-                        />
-                    @endif
-                    <div>
-                        <p
-                            class="text-[20px] font-medium text-black md:text-2xl dark:text-white"
-                        >
-                            {{ $author['name'] }}
-                        </p>
-                        <div
-                            class="mt-2 text-zinc-600 md:mt-3 dark:text-zinc-400"
-                        >
-                            <p class="dark">
-                                {{ $author['bio'] }}
-                            </p>
-                        </div>
-                        <a
-                            class="hover:text-primary dark:hover:text-primary-dark mt-3 flex w-fit items-center gap-x-1 text-sm font-medium underline md:text-base dark:text-zinc-200"
-                            href="{{ route('prezet.index', ['author' => strtolower($document->frontmatter->author)]) }}"
-                        >
-                            More posts from {{ $author['name'] }}
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="size-4"
+                @if($author['bio'])
+                    <div
+                        id="author"
+                        class="flex flex-col items-start gap-x-6 gap-y-4 rounded-xl bg-zinc-50 p-6 ring-1 ring-zinc-500/10 ring-inset md:flex-row md:p-7 dark:bg-zinc-800 dark:text-zinc-300"
+                    >
+                        @if($author['image'])
+                            <img
+                                src="{{ $author['image'] }}"
+                                alt="profile image of {{ $author['name'] }}"
+                                width="135"
+                                height="135"
+                                loading="lazy"
+                                decoding="async"
+                                class="h-24 w-24 rounded-xl bg-zinc-100 object-cover md:h-[135px] md:w-[135px] dark:bg-zinc-800"
+                            />
+                        @endif
+                        <div>
+                            <p
+                                class="text-[20px] font-medium text-black md:text-2xl dark:text-white"
                             >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                                />
-                            </svg>
-                        </a>
+                                {{ $author['name'] }}
+                            </p>
+                            <div
+                                class="mt-2 text-zinc-600 md:mt-3 dark:text-zinc-400"
+                            >
+                                <p class="dark">
+                                    {{ $author['bio'] }}
+                                </p>
+                            </div>
+                            <a
+                                class="hover:text-primary dark:hover:text-primary-dark mt-3 flex w-fit items-center gap-x-1 text-sm font-medium underline md:text-base dark:text-zinc-200"
+                                href="{{ route('prezet.index', ['author' => strtolower($document->frontmatter->author)]) }}"
+                            >
+                                More posts from {{ $author['name'] }}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    class="size-4"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                                    />
+                                </svg>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                @endif
             </div>
         </div>
     </x-prezet.alpine>
