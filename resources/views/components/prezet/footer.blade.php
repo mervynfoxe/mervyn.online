1340a4b42f44e46c5b72b4d85cb07b17e1576bd6
Mervyn Fox <dev@mervyn.online>
2025-07-30 22:09:28 -0400

    Add feed link

--- resources/views/components/prezet/footer.blade.php
+++ resources/views/components/prezet/footer.blade.php
@@ -7,30 +7,29 @@
         >
             <div class="relative flex gap-4 justify-between grow basis-0 items-center text-sm text-gray-900 dark:text-gray-100">
                 <div>
-                &copy;2014-<?= date("Y") ?> Ren Mervyn Fox
+                    &copy;2014-<?= date("Y") ?> Ren Mervyn Fox
                 </div>
                 <div class="grow">
-                    <nav class="flex gap-4">
+                    <nav class="flex gap-4 items-center">
                         <a
                             href="{{ route('prezet.index') }}/about"
                             class="inline-block"
                         >
-                    <span
-                        class="text-gray-900 dark:text-gray-300"
-                    >
-                        About
-                    </span>
+                            <span class="text-gray-900 dark:text-gray-300">
+                                About
+                            </span>
                         </a>
                         <a
                             aria-label="Home"
                             href="{{ route('prezet.index') }}/shoutbox"
                             class="inline-block"
                         >
-                    <span
-                        class="text-gray-900 dark:text-gray-300"
-                    >
-                        Shoutbox
-                    </span>
+                            <span class="text-gray-900 dark:text-gray-300">
+                                Shoutbox
+                            </span>
+                        </a>
+                        <a class="inline-block w-4" href="{{ route('feeds.main') }}">
+                            <img class="sidebar-icon" src="{{ Vite::asset('resources/images/icons/rss.svg') }}" alt="RSS feed icon" />
                         </a>
                     </nav>
                 </div>
